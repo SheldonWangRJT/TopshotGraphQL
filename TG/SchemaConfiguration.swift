de1e617746d21aa69c8b1a66ae0e361aa02f7bb1
Engineer Sheldon Wang <daniel.wangxiao@gmail.com>
2023-05-25 12:51:35 -0400

    Update SchemaConfiguration.swift

--- TG/SchemaConfiguration.swift
+++ TG/SchemaConfiguration.swift
@@ -8,8 +8,7 @@
 import Apollo
 
 enum SchemaConfiguration: Apollo.SchemaConfiguration {
-  static func cacheKeyInfo(for type: Object, object: JSONObject) -> CacheKeyInfo? {
-    // Implement this function to configure cache key resolution for your schema types.
-    return nil
-  }
+    static func cacheKeyInfo(for type: Apollo.Object, object: Apollo.ObjectData) -> Apollo.CacheKeyInfo? {
+        return nil
+    }
 }
